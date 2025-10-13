c0cad2f3064f4fb69f6af7ec5505645dde2fb16b
ldev <ldev@ldev.eu.org>
2025-10-13 19:24:59 +0200

    templates eindelijk gefixt

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,20 +13,44 @@ type les struct {
 	Id   int
 }
 
+func createHTMLRenderer(rootDir string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	globals := []string{rootDir + "/base.tmpl"}
+
+	pages, err := filepath.Glob(rootDir + "/pages/*.tmpl")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, page := range pages {
+		files := make([]string, len(globals)+1)
+		copy(files, globals)
+		files[len(files)-1] = page
+		r.AddFromFiles(filepath.Base(page), files...)
+	}
+	return r
+}
+
 func main() {
 	r := gin.Default()
-	r.LoadHTMLFiles("./www/templates/base.html", "./www/templates/pages/home.html")
-	r.LoadHTMLGlob("./www/templates/*")
-
+	r.HTMLRender = createHTMLRenderer("./www/templates")
 	r.Static("/static", "./www/static")
+
 	r.GET("/", func(c *gin.Context) {
 		lessen := []les{
 			les{Id: 0, Name: "test les 1"},
 			les{Id: 1, Name: "test les 2"},
 			les{Id: 2, Name: "test les 3"},
 		}
-		c.HTML(http.StatusOK, "pages/home.html", lessen)
+		c.HTML(http.StatusOK, "home.tmpl", lessen)
 
+	})
+
+	r.GET("/lesson/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		c.HTML(http.StatusOK, "lesson.tmpl", id)
 	})
 
 	r.Run("localhost:2025")
